b2420329c11772004cb36b881543fe46f405574b
maxim <horbatyukitstep@gmail.com>
2022-01-31 17:21:18 +0200

    Class Line and Figures array added

--- Project/main.cpp
+++ Project/main.cpp
@@ -52,10 +52,27 @@ public:
 
 };
 
-int main() {
+class Line : public Figure {
+public:
+	Line(Point one, Point two) :Figure(2, one, two) {
 
-	Square s({ Point(1, 2), Point(3, 5), Point(1, 2), Point(5, 5) });
-	s.print();
+	}
+
+	void print() override {
+		cout << "Line:\n";
+		cout << "------------------------------------------------------------\n";
+		for (auto el : points)
+			el.print();
+		cout << "------------------------------------------------------------\n";
+	}
+
+};
+
+int main() {
+	const int size = 2;
+	Figure* figures[2]{ new Square({Point(1, 2), Point(3, 5), Point(1, 2), Point(5, 5)}), new Line(Point(1,2),Point(1,5)) };
+	for (int i = 0; i < size; i++)
+		figures[i]->print();
 	
 
 	return 0;
