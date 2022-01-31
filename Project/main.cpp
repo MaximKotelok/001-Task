7db749e5a7a11b44bf9885605cc7ce38410bc003
maxim <horbatyukitstep@gmail.com>
2022-01-31 16:22:51 +0200

    Class point added

--- Project/main.cpp
+++ Project/main.cpp
@@ -2,7 +2,24 @@
 
 using namespace std;
 
+class Point {
+	int x, y;
+public:
+
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	void print() {
+		cout << "X = " << x << " | " << "Y = " << y << "\n";
+	}
+};
+
 int main() {
+	Point one(1, 2);
+	one.print();
+
 
 	return 0;
 }
